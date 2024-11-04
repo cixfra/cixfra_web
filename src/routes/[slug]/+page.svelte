4f003877cbfecd2b2ee4cbd130138f8fc9a4b34b
RubenPX <rubenmarchante@gmail.com>
2024-11-04 16:40:57 +0100

    Fallback error page

---
+++ src/routes/[slug]/+page.svelte
@@ -0,0 +1,15 @@
+<script>
+	import { page } from "$app/stores";
+	function fuckGoBack() { history.back() }
+</script>
+
+<div id="content">
+	<h1>OwO what's this?!</h1>
+	<p>Looks like you got yourself into a problem ({$page.status}: {$page.error?.message})</p>
+	<button on:click={fuckGoBack}>Take me back pwease &gtw&lt</button>
+</div>
+
+<style>
+	@import "../JetBrainsMono.css";
+	@import "../error.css";
+</style>
